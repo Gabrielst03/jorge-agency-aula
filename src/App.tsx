79b9f5c7f49767a45df9e877e8f7fe1d3851c6bd
Gabriel Santana <santanag311@gmail.com>
2024-01-25 11:37:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Clientes from "./components/Clientes";
+
+export default function App() {
+  return (
+    <main className="flex flex-col min-h-screen py-4">
+      <Header />
+      <Hero />
+      <Clientes />
+    </main>
+  )
+}
